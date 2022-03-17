0031493cb3599a58b32407da2c3fd97b03f15ef6
Fasten90 <fasten90@gmail.com>
2022-03-18 00:27:34 +0100

    Added more tests for system test execution

--- test_exec.sh
+++ test_exec.sh
@@ -1,3 +1,6 @@
 echo "Test Exec" 
 
 python FastenStaticAnalyzer.py --source="test/test_goto.c" --prepocessor="gcc" --preprocessor_args=""
+python FastenStaticAnalyzer.py --source="test/test_2/test.c" --prepocessor="gcc" --preprocessor_args=""
+python FastenStaticAnalyzer.py --source="test/test_simple_without_stdio/test_simple_without_stdio.c" --prepocessor="gcc" --preprocessor_args=""
+python FastenStaticAnalyzer.py --source="test/test_stdio/test_stdio.c" --prepocessor="gcc" --preprocessor_args=""
