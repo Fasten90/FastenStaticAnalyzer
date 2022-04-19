24c8fba8d9e7cdfe808f5200f4d22cb18c9b10e2
Fasten90 <fasten90@gmail.com>
2022-04-20 01:00:13 +0200

    Try to fix system test

--- test_exec.sh
+++ test_exec.sh
@@ -8,4 +8,4 @@ python FastenStaticAnalyzer.py --source="test/test_1_goto/test_goto.c" --preproc
 python FastenStaticAnalyzer.py --source="test/test_2/test_2.c" --preprocessor="gcc" --preprocessor_args="-std=c99 -nostdinc -E -D_Atomic(_arg)=_arg -Ipycparser/utils/fake_libc_include"
 python FastenStaticAnalyzer.py --source="test/test_simple_without_stdio/test_simple_without_stdio.c" --preprocessor="gcc" --preprocessor_args=""
 python FastenStaticAnalyzer.py --source="test/test_stdio/test_stdio.c" --preprocessor="gcc" --preprocessor_args="-std=c99 -nostdinc -E -D_Atomic(_arg)=_arg -Ipycparser/utils/fake_libc_include"
-python FastenStaticAnalyzer.py --source="test/test_system_call_3/test.c" --preprocessor="gcc" --preprocessor_args="" --delete_temporary_files --export_file=bla_result_2.txt
+python FastenStaticAnalyzer.py --source="test/test_system_call_3/test.c" --preprocessor="gcc" --preprocessor_args="-std=c99 -nostdinc -E -D_Atomic(_arg)=_arg -Ipycparser/utils/fake_libc_include" --delete_temporary_files --export_file=bla_result_2.txt
