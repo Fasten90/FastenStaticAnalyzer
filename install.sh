a5e025188d336858650aaab20a97f91036825009
Gabor Vizi <Gabor_Vizi@mentor.com>
2025-11-16 18:02:15 +0100

    Fix - python3 pip if missed

--- install.sh
+++ install.sh
@@ -13,4 +13,9 @@ fi
 #python -m venv venv
 
 echo "Install requirements"
-python3 -m pip install -r requirements.txt
+
+python3_output=$(python3 -m pip install -r requirements.txt)
+python3_result=$?
+if [ $python3_result -ne 0 ]; then
+    pip install -r requirements.txt
+fi
